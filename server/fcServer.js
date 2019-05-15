ee0205b696ea02be8a1a3bd3b2147580a7f6cee2
Lei Zuo <lzuo98@gmail.com>
2019-05-14 20:10:42 -0700

    finished Using the API

--- server/fcServer.js
+++ server/fcServer.js
@@ -13,12 +13,60 @@ const port = 59353
 
 
 
-fucntion translateHandler(req, res, next) {
+function translateHandler(req, res, next) {
     let url = req.url;
     let qObj = req.query;
     console.log(qObj);
     if(qObj.english != undefined) {
-        issueAPIrequest(qObj.english);
+        // API request
+        let requestObject =
+            {
+              "source": "en",
+              "target": "zh-CN",
+              "q": [
+                qObj.english
+              ]
+            }
+        console.log("English phrase: ", requestObject.q[0]);
+
+
+        // callback function, called when data is received from API
+        function APIcallback(err, APIresHead, APIresBody) {
+            // gets three objects as input
+            if ((err) || (APIresHead.statusCode != 200)) {
+                // API is not working
+                console.log("Got API error");
+                console.log(APIresBody);
+            } else {
+                if (APIresHead.error) {
+                    // API worked but is not giving you data
+                    console.log(APIresHead.error);
+                } else {
+                    console.log("In Korean: ", APIresBody.data.translations[0].translatedText);
+                    console.log("\n\nJSON was:");
+                    console.log(JSON.stringify(APIresBody, undefined, 2));
+                    // print it out as a string, nicely formatted
+
+                    res.json({"English" : qObj.english, "Korean" : APIresBody.data.translations[0].translatedText});
+                }
+            }
+        } // end callback function
+
+
+        // The call that makes a request to the API
+        // Uses the Node request module, which packs up and sends off
+        // an HTTP message containing the request to the API server
+        APIrequest(
+        	{ // HTTP header stuff
+        	    url: apiurl,
+        	    method: "POST",
+        	    headers: {"content-type": "application/json"},
+        	    // will turn the given object into JSON
+        	    json: requestObject	},
+        	// callback function for API request
+        	APIcallback
+            );
+
     } else {
         next();
     }
@@ -34,7 +82,7 @@ function fileNotFound(req, res) {
 // put together the server pipeline
 const app = express()
 app.use(express.static('public'));  // can I find a static file?
-app.get('/translate', translateHandler );   // if not, is it a valid query?
+app.get('/translate', translateHandler);   // if not, is it a valid query?
 app.use( fileNotFound );            // otherwise not found
 
 app.listen(port, function (){console.log('Listening...');} )
@@ -44,50 +92,30 @@ app.listen(port, function (){console.log('Listening...');} )
 
 // API
 
-function issueAPIrequest(entext) {
-
-  let requestObject =
-      {
-        "source": "en",
-        "target": "ko",
-        "q": [
-          entext
-        ]
-      }
-  console.log("English phrase: ", requestObject.q[0]);
-
-
-  // The call that makes a request to the API
-  // Uses the Node request module, which packs up and sends off
-  // an HTTP message containing the request to the API server
-  APIrequest(
-  	{ // HTTP header stuff
-  	    url: url,
-  	    method: "POST",
-  	    headers: {"content-type": "application/json"},
-  	    // will turn the given object into JSON
-  	    json: requestObject	},
-  	// callback function for API request
-  	APIcallback
-      );
-}
-
-// callback function, called when data is received from API
-function APIcallback(err, APIresHead, APIresBody) {
-    // gets three objects as input
-    if ((err) || (APIresHead.statusCode != 200)) {
-        // API is not working
-        console.log("Got API error");
-        console.log(APIresBody);
-    } else {
-        if (APIresHead.error) {
-            // API worked but is not giving you data
-            console.log(APIresHead.error);
-        } else {
-            console.log("In Korean: ", APIresBody.data.translations[0].translatedText);
-            console.log("\n\nJSON was:");
-            console.log(JSON.stringify(APIresBody, undefined, 2));
-            // print it out as a string, nicely formatted
-        }
-    }
-} // end callback function
+// function issueAPIrequest(entext) {
+//
+//   let requestObject =
+//       {
+//         "source": "en",
+//         "target": "ko",
+//         "q": [
+//           entext
+//         ]
+//       }
+//   console.log("English phrase: ", requestObject.q[0]);
+//
+//
+//   // The call that makes a request to the API
+//   // Uses the Node request module, which packs up and sends off
+//   // an HTTP message containing the request to the API server
+//   APIrequest(
+//   	{ // HTTP header stuff
+//   	    url: apiurl,
+//   	    method: "POST",
+//   	    headers: {"content-type": "application/json"},
+//   	    // will turn the given object into JSON
+//   	    json: requestObject	},
+//   	// callback function for API request
+//   	APIcallback
+//       );
+// }
