20176bd3e7e3815fc6f2d2eb727a6d8be6dca705
Lei Zuo <lzuo98@gmail.com>
2019-05-16 17:35:44 -0700

    main page css TODO

--- server/public/lango.js
+++ server/public/lango.js
@@ -1,58 +1,71 @@
 'use strict';
 
+// Landing Page
+
 var landing_page = React.createElement(
-	"main",
+	'main',
 	null,
 	React.createElement(
-		"p",
+		'p',
 		{ onClick: GoMainPage },
-		"Click!"
+		'Click!'
 	)
 );
 
+// Main Page
+
+function GoMainPage() {
+	ReactDOM.render(main_page, document.getElementById('root'));
+}
+
 // An element to go into the DOM
 var lango = React.createElement(
-	"h1",
-	{ id: "logo" },
-	"Lango!"
+	'h1',
+	{ id: 'logo' },
+	'Lango!'
 );
 
 // A component - function that returns some elements
-function FirstCard() {
+function MainEnglishCard() {
 	return React.createElement(
-		"div",
-		{ className: "textCard" },
-		React.createElement(
-			"p",
-			null,
-			"Hello, world!"
-		)
+		'div',
+		{ className: 'textCard' },
+		React.createElement('textarea', { id: 'mainLeft', onKeyPress: MainLeftReturn })
 	);
 }
 
 // Another component
-function FirstInputCard() {
+function MainTranslationCard() {
 	return React.createElement(
-		"div",
-		{ className: "textCard" },
-		React.createElement("textarea", { onKeyPress: checkReturn })
+		'div',
+		{ className: 'textCard' },
+		React.createElement(
+			'p',
+			{ id: 'mainRight' },
+			'Hello, world!'
+		)
 	);
+}
+
+function MainLeftReturn(event) {
+	if (event.charCode == 13) {
+		document.getElementById("mainRight").textContent = "Goodbye world!";
+		makeTranslateRequest();
+	}
 }
 
 // An element with some contents, including a variable
 // that has to be evaluated to get an element, and some
 // functions that have to be run to get elements.
-var main = React.createElement(
-	"main",
+var main_page = React.createElement(
+	'main',
 	null,
 	lango,
-	React.createElement(FirstInputCard, null),
-	React.createElement(FirstCard, null)
+	React.createElement(MainEnglishCard, null),
+	React.createElement(MainTranslationCard, null)
 );
 
-function GoMainPage() {
-	ReactDOM.render(main, document.getElementById('root'));
-}
+// Default render : landing page
 
 ReactDOM.render(landing_page, document.getElementById('root'));
 
@@ -60,4 +73,44 @@ ReactDOM.render(landing_page, document.getElementById('root'));
 // When the charCode is 13, the user has hit the return key
 function checkReturn(event) {
 	console.log(event.charCode);
+}
+
+// Create the XHR object.
+function createCORSRequest(method, url) {
+	var xhr = new XMLHttpRequest();
+	xhr.open(method, url, true); // call its open method
+	return xhr;
+}
+
+// Make the actual CORS request.
+function makeTranslateRequest() {
+
+	var url = "http://server162.site:59353/translate?english=" + document.getElementById("mainLeft").value;
+
+	var xhr = createCORSRequest('GET', url);
+
+	// checking if browser does CORS
+	if (!xhr) {
+		alert('CORS not supported');
+		return;
+	}
+
+	// Load some functions into response handlers.
+	xhr.onload = function () {
+		var responseStr = xhr.responseText; // get the JSON string
+		var object = JSON.parse(responseStr); // turn it into an object
+		console.log(JSON.stringify(object, undefined, 2)); // print it out as a string, nicely formatted
+		updateMainRight(object);
+	};
+
+	xhr.onerror = function () {
+		alert('Woops, there was an error making the request.');
+	};
+
+	// Actually send request to server
+	xhr.send();
+}
+
+function updateMainRight(object) {
+	document.getElementById("mainRight").textContent = object.data.translations[0].translatedText;
 }
--- server/public/src/lango.jsx
+++ server/public/src/lango.jsx
@@ -1,5 +1,7 @@
 'use strict';
 
+// Landing Page
+
 const landing_page = (<main>
 				<p onClick={GoMainPage}>Click!</p>
 
@@ -7,41 +9,53 @@ const landing_page = (<main>
 
 		</main>);
 
+// Main Page
+
+function GoMainPage() {
+			ReactDOM.render(
+			    main_page,
+			    document.getElementById('root')
+			);
+		}
+
 // An element to go into the DOM
 const lango = <h1 id="logo">Lango!</h1>;
 
-
 // A component - function that returns some elements
-function FirstCard() {
+function MainEnglishCard() {
 	 return (<div className="textCard">
-	 <p>Hello, world!</p>
+	 <textarea id="mainLeft" onKeyPress={MainLeftReturn} />
 	 </div>);
 	 }
 
 // Another component
-function FirstInputCard() {
+function MainTranslationCard() {
          return (<div className="textCard">
-	          <textarea onKeyPress={checkReturn} />
+				 			<p id="mainRight">Hello, world!</p>
 		  </div>);
             }
+
+function MainLeftReturn(event) {
+		if(event.charCode == 13) {
+			document.getElementById("mainRight").textContent = "Goodbye world!";
+			makeTranslateRequest();
+		}
+}
 
 // An element with some contents, including a variable
 // that has to be evaluated to get an element, and some
 // functions that have to be run to get elements.
-const main = (<main>
+const main_page = (<main>
 		{lango}
-	      	<FirstInputCard/>
-	      	<FirstCard />
+	      	<MainEnglishCard />
+	      	<MainTranslationCard />
 	      </main>
 	     );
 
-function GoMainPage() {
-	ReactDOM.render(
-	    main,
-	    document.getElementById('root')
-	);
-}
 
+
+
+// Default render : landing page
 
 ReactDOM.render(
     landing_page,
@@ -53,3 +67,49 @@ ReactDOM.render(
 function checkReturn(event) {
 	 console.log(event.charCode);
 	 }
+
+
+
+
+
+
+
+	 // Create the XHR object.
+function createCORSRequest(method, url) {
+	   let xhr = new XMLHttpRequest();
+	   xhr.open(method, url, true);  // call its open method
+	   return xhr;
+}
+
+	 // Make the actual CORS request.
+function makeTranslateRequest() {
+
+	   let url = "http://server162.site:59353/translate?english=" + document.getElementById("mainLeft").value;
+
+	   let xhr = createCORSRequest('GET', url);
+
+	   // checking if browser does CORS
+	   if (!xhr) {
+	     alert('CORS not supported');
+	     return;
+	   }
+
+	   // Load some functions into response handlers.
+	   xhr.onload = function() {
+	       let responseStr = xhr.responseText;  // get the JSON string
+	       let object = JSON.parse(responseStr);  // turn it into an object
+	       console.log(JSON.stringify(object, undefined, 2));  // print it out as a string, nicely formatted
+	       updateMainRight(object);
+	   };
+
+	   xhr.onerror = function() {
+	     alert('Woops, there was an error making the request.');
+	   };
+
+	   // Actually send request to server
+	   xhr.send();
+}
+
+function updateMainRight(object) {
+	     document.getElementById("mainRight").textContent = object.data.translations[0].translatedText;
+}
